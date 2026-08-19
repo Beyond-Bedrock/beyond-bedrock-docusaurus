8b52f31eee1351c1316c69779d9a7ec0f379c9fe
Minato <minecraftbedrockarabic@gmail.com>
2026-08-19 13:43:20 +0100

    Update counterClient.js

--- src/lib/counterClient.js
+++ src/lib/counterClient.js
@@ -1,6 +1,4 @@
 const proxyUrl = 'https://docs-counter-proxy.weathered-dew-2d87.workers.dev';
-const supabaseUrl = 'https://tedcnrtzdznipuljtvke.supabase.co';
-const supabaseAnonKey = 'sb_publishable_T5VoG4fAkJmNCqS--9zOPg_R_tYvP_t';
 
 async function callCounter(fn, args, keepalive = false) {
   const res = await fetch(proxyUrl, {
@@ -25,37 +23,11 @@ export function incrementDownload(file) {
   return callCounter('increment_download', { file }, true);
 }
 
-// Get count without incrementing - direct Supabase query with anon key
+// Get count without incrementing - use Worker to avoid CSP issues
 export function getPageViewCount(pageSlug) {
-  return fetch(`${supabaseUrl}/rest/v1/page_views?slug=eq.${encodeURIComponent(pageSlug)}&select=count`, {
-    headers: {
-      'apikey': supabaseAnonKey,
-      'Authorization': `Bearer ${supabaseAnonKey}`,
-    },
-  })
-  .then(res => res.json())
-  .then(data => {
-    if (Array.isArray(data) && data.length > 0) {
-      return data[0].count;
-    }
-    return null;
-  })
-  .catch(() => null);
+  return callCounter('get_page_view', { page_slug: pageSlug });
 }
 
 export function getDownloadCount(file) {
-  return fetch(`${supabaseUrl}/rest/v1/file_downloads?file_id=eq.${encodeURIComponent(file)}&select=count`, {
-    headers: {
-      'apikey': supabaseAnonKey,
-      'Authorization': `Bearer ${supabaseAnonKey}`,
-    },
-  })
-  .then(res => res.json())
-  .then(data => {
-    if (Array.isArray(data) && data.length > 0) {
-      return data[0].count;
-    }
-    return null;
-  })
-  .catch(() => null);
+  return callCounter('get_download_count', { file });
 }
