561908acfffb1ddab01f426c1db32af6eadaba60
Minato <minecraftbedrockarabic@gmail.com>
2026-08-19 13:57:01 +0100

    Update index.js

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -13,10 +13,12 @@ export default function LayoutWrapper(props) {
           crossOrigin="anonymous"
         />
       </Head>
-      <Layout {...props} />
-      <div style={{ marginTop: '1rem', fontSize: '0.9rem', color: 'var(--ifm-color-secondary)', textAlign: 'center', paddingBottom: '2rem' }}>
-        <ViewCounter incrementOnMount={true} showLabel={true} />
-      </div>
+      <Layout {...props}>
+        {props.children}
+        <div style={{ marginTop: '1rem', fontSize: '0.9rem', color: 'var(--ifm-color-secondary)', textAlign: 'center', paddingBottom: '2rem' }}>
+          <ViewCounter incrementOnMount={true} showLabel={true} />
+        </div>
+      </Layout>
     </>
   );
 }
